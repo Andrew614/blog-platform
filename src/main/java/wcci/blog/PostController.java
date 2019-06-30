a06894e7351dc7d481555ad670616afe1dfdea38
tyler-conner <tyler.conner@gmail.com>
2019-06-30 12:26:55 -0400

    fixed controller issue

--- src/main/java/wcci/blog/PostController.java
+++ src/main/java/wcci/blog/PostController.java
@@ -1,10 +1,11 @@
 package wcci.blog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class PostController {
 
 	@Autowired
